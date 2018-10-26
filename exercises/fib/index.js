50ab5a7f4c5861ef1c2e2001c50e062d262a1e0c
Matthew Burnside <matthewsburnside@gmail.com>
2018-10-26 15:09:15 -0700

    Solved the fibonacci sequence with recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -12,14 +12,26 @@
 // // Linear Time Solution
 // // As n is increased, the loop (which is all of the work in this case) must run 1 additional time.
 // // The prefered solution
-function fib(n) {
-    let sequence = [0, 1];
+// function fib(n) {
+//     let sequence = [0, 1];
 
-    for (var i = 2; i <= n; i++) {
-        sequence.push(sequence[i - 2] + sequence[i - 1]);
+//     for (var i = 2; i <= n; i++) {
+//         sequence.push(sequence[i - 2] + sequence[i - 1]);
+//     }
+
+//     return sequence[n];
+// }
+
+// // Recursive Solution
+// // Exponential Time Solution
+// // With recursion, the sought value is broken down into pieces (n - 1) and (n - 2) until left with a collection of fib(0) and fib(1) (or 0 and 1). Those added together have given us our value. When you increase the initial n (the position of the value in question) the amount of time fib() must be called increases exponentially.
+// // A terrible solution to actually use
+function fib(n) {
+    if (n < 2 ) {
+        return n;
     }
 
-    return sequence[n];
+    return fib(n - 1) + fib(n - 2);
 }
 
 module.exports = fib;
