cda3e4bbafadcf6e850eb8c014a4fba736151b35
Matthew Burnside <matthewsburnside@gmail.com>
2018-10-31 17:48:39 -0700

    Created a Queue Data Structure using 2 Stack Data Structures

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,43 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.firstStack = new Stack;
+        this.secondStack = new Stack;
+    }
+
+    add(record) {
+        this.firstStack.push(record);
+    }
+
+    remove() {
+        while (this.firstStack.peek()) {
+            this.secondStack.push(this.firstStack.pop());
+        }
+
+        const record = this.secondStack.pop();
+
+        while (this.secondStack.peek()) {
+            this.firstStack.push(this.secondStack.pop());
+        }
+
+        return record;
+    }
+
+    peek() {
+        while (this.firstStack.peek()) {
+            this.secondStack.push(this.firstStack.pop());
+        }
+
+        const record = this.secondStack.peek();
+
+        while (this.secondStack.peek()) {
+            this.firstStack.push(this.secondStack.pop());
+        }
+
+        return record;
+    }
+}
 
 module.exports = Queue;
