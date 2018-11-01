d9f5f722fd7401cf919290066081d4f4fac053e8
Matthew Burnside <matthewsburnside@gmail.com>
2018-11-01 12:53:37 -0700

    Added method for return the size of the linked list.

--- exercises/linkedlist/test.js
+++ exercises/linkedlist/test.js
@@ -18,7 +18,7 @@ describe.skip('A Node', () => {
   });
 });
 
-describe('Insert First', () => {
+describe.skip('Insert First', () => {
   test('appends a node to the start of the list', () => {
     const l = new List();
     l.insertFirst(1);
@@ -28,7 +28,7 @@ describe('Insert First', () => {
   });
 });
 
-describe.skip('Size', () => {
+describe('Size', () => {
   test('returns the number of items in the linked list', () => {
     const l = new List();
     expect(l.size()).toEqual(0);
