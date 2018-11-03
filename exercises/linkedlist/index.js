1d27c9e7b58a6a9e213f2125d0faf616e1bf6f3c
Matthew Burnside <matthewsburnside@gmail.com>
2018-11-02 17:53:19 -0700

    Reorganized and simplified methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -14,10 +14,6 @@ class LinkedList {
         this.head = null;
     }
 
-    insertFirst(data) {
-        this.head = new Node(data, this.head);
-    }
-
     size() {
         let counter = 0;
         let node = this.head;
@@ -30,65 +26,8 @@ class LinkedList {
         return counter;
     }
 
-    // getHead()
-    getFirst() {
-        return this.head;
-    }
-
-    // getTail()
-    getLast() {
-        if (!this.head) {
-            return null;
-        }
-
-        let node = this.head;
-        while (node) {
-            if (!node.next) {
-                return node;
-            }
-            node = node.next;
-        }
-    }
-
     clear() {
         this.head = null;
-    }
-
-    removeFirst() {
-        if (!this.head) {
-            return;
-        }
-
-        this.head = this.head.next;
-    }
-
-    removeLast() {
-        if (!this.head) {
-            return;
-        }
-
-        if (!this.head.next){
-            this.head = null;
-            return;
-        }
-
-        let previous = this.head;
-        let node = this.head.next;
-        while (node.next) {
-            previous = node;
-            node = node.next;
-        }
-        previous.next = null;
-    }
-
-    insertLast(data) {
-        const last = this.getLast();
-
-        if (last) {
-            last.next = new Node(data);
-        } else {
-            this.head = new Node(data);
-        }
     }
 
     getAt(index) {
@@ -138,6 +77,73 @@ class LinkedList {
         const previous = this.getAt(index - 1) || this.getLast();
         const node = new Node(data, previous.next);
         previous.next = node;
+    }
+
+    // getHead()
+    getFirst() {
+        return this.head;
+        // return this.getAt(0);
+    }
+
+    // getTail()
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+        // Simplifiying by reusing getAt method needs to run size(), thus making it less efficient due to 2 loops
+    }
+
+    removeFirst() {
+        // if (!this.head) {
+        //     return;
+        // }
+
+        // this.head = this.head.next;
+        this.removeAt(0);
+    }
+
+    removeLast() {
+        if (!this.head) {
+            return;
+        }
+
+        if (!this.head.next){
+            this.head = null;
+            return;
+        }
+
+        let previous = this.head;
+        let node = this.head.next;
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+        previous.next = null;
+        // Simplifiying by reusing removeAt method needs to run size(), thus making it less efficient due to the extra loop
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        // this.insertAt(data, 0);
+    }
+
+    insertLast(data) {
+        const last = this.getLast();
+
+        if (last) {
+            last.next = new Node(data);
+        } else {
+            this.head = new Node(data);
+        }
+        // Simplifiying by reusing insertAt method needs to run size(), thus making it less efficient due to the extra loop
     }
 }
 
