2d2485c378557e3bbe7f7ef9cf1699ffc555c0ce
Matthew Burnside <matthewsburnside@gmail.com>
2018-12-12 14:52:44 -0800

    Added time complexity notes for sorting algorithms

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,14 +2,19 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+    // WORST CASE RUNTIME: n^2
 
 }
 
 function selectionSort(arr) {
+    // WORST CASE RUNTIME: n^2
+    // This requires significantly more work as the data set grows. ONLY USE FOR KNOWN SMALL DATA SETS
 
 }
 
 function mergeSort(arr) {
+    // WORST CASE RUNTIME: n*log(n)
+    // This requires slightly more work as the data set grows. USE FOR LARGER OR UNKOWN DATA SET SIZES
 
 }
 
