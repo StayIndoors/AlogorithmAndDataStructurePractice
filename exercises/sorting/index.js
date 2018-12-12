94f4e29d6874b6d0d57456eafc7a750c72831f13
Matthew Burnside <matthewsburnside@gmail.com>
2018-12-12 15:16:16 -0800

    Wrote a bubblesort algorithm

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,7 +3,16 @@
 
 function bubbleSort(arr) {
     // WORST CASE RUNTIME: n^2
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length - i - 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr
 }
 
 function selectionSort(arr) {
